09dc0df473a2620c4c92f66642c4baa5288d7f08
Erik Amyotte <eamyotte+git@proton.me>
2024-03-22 21:55:49 -0600

    add scoring system to game

--- game.js
+++ game.js
@@ -18,6 +18,18 @@ const scissors = "Scissors"
 let playerChoice
 let computerChoice
 
+//Variables for tracking scores and rounds
+let roundsPlayed = 0;
+let playerScore = 0;
+let computerScore = 0;
+let draws = 0;
+let scoreCard = " "
+
+//Function to update score card dynamically
+function updateScoreCard(){
+    scoreCard = "Round: " + roundsPlayed + " Player: " + playerScore + " Computer: " + computerScore + " Tie: " + draws;
+}
+
 //function to prompt for players choice
 function getPlayerChoice(){
     let choose = prompt("Choose: 'Rock',Paper','Scissors");
@@ -29,7 +41,9 @@ function getPlayerChoice(){
         }
         else if (choose.toLowerCase() === "scissors"){
         playerChoice = scissors}
-        else alert("You suck")
+        else{ alert("Invalid. Please try again.")// Handles invalid user input and re-prompts
+        getPlayerChoice();
+        }
 }
 
 //function to generate computers choice
@@ -50,19 +64,47 @@ function declareWinner(){
     if (playerChoice && computerChoice){
         console.log("Player: " + playerChoice);
         console.log("Computer: " + computerChoice);
+        roundsPlayed++;
+        
     }
     if (playerChoice === computerChoice){
-    console.log("It's a tie")}
+        draws++;
+        updateScoreCard();
+    console.log("It's a tie")
+    console.log(scoreCard)
+}
 else if(
     (playerChoice === rock  && computerChoice === scissors) ||
     (playerChoice === paper && computerChoice === rock) ||
     (playerChoice === scissors && computerChoice === paper)
     ){
+        playerScore++;
+        updateScoreCard();
         console.log("You win!")
+        console.log(scoreCard)
     }
-    else {console.log("Computer Wins!")}
+    else {
+        computerScore++;
+        updateScoreCard();
+        console.log("Computer Wins!")
+        console.log(scoreCard)
+    }
 }
+//function to play again
+function playAgain (){
+    var anotherRound = (confirm ("Would you like to play another round?"))
+        if (anotherRound === true){
+            getPlayerChoice();
+            getComputerChoice();
+            declareWinner();
+            playAgain();
+        }
+        else{
+            console.log("Goodbye")
+        }
+    }
 //run the game
 getPlayerChoice();
 getComputerChoice();
 declareWinner();
+playAgain();
